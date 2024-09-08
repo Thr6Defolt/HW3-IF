1f8e584eef22228e53d939b738d12c9bded2d7c4
Sitnik <Sitnik@DESKTOP-O04FKLD>
2024-09-08 10:27:07 +0300

    Add project files.

---
+++ HW3 IF/HW3 IF.cpp
@@ -0,0 +1,180 @@
+
+#include <iostream>
+
+int main()
+{
+
+    //file 1 task 1
+
+    /*
+    int num = 0;
+    std::cout << "Print a number";
+    std::cin >> num;
+    if (num % 2 == 0)
+    {
+        std::cout << "It a count number";
+    }      
+    else
+    {
+        std::cout << "It a uncount number";
+    }  
+    */
+
+    //file 1 task 3
+
+    /*
+    int num = 0;
+    std::cout << "Print a number";
+    std::cin >> num;
+    if (num == 0)
+    {
+        std::cout << "It a Zero number";
+    }
+    else if (num < 0)
+    {
+        std::cout << "It a NEgativ number";
+    }
+    else;
+    {
+        std::cout << "It a Positiv number";
+    }
+    */
+
+    //file 1 task 4
+
+    /*
+    int num1 = 0;
+    int num2 = 0;
+    int op = 0;
+    std::cout << "Print a first number";
+    std::cin >> num1;
+    std::cout << "Print a second number";
+    std::cin >> num2;
+    std::cout << "Print a operation 1)+ 2)- 3)/ 4)*";
+    std::cin >> op;
+    if (op == 1)
+    {
+        std::cout << num1 + num2;
+    }
+    else if (op == 2)
+    {
+        std::cout << num1 - num2;
+    }
+    else if (op == 3)
+    {
+        std::cout << num1 / num2;
+    }
+    else if (op == 4)
+    {
+        std::cout << num1 * num2;
+    }
+    */
+
+    //file 1 task 6
+
+    /*
+    int num = 0;
+    std::cout << "Print a number";
+    std::cin >> num;
+    if (num >= 6 && num <= 78)
+    {
+        std::cout << "In Diapozon";
+    }
+    else;
+    {
+        std::cout << "Not in IDapazon";
+    }
+    */
+
+    //file 1 task 7
+
+    /*
+    int x = 0;
+    std::cout << "Print a x";
+    std::cin >> x;
+    int y = 0;
+    std::cout << "Print a y";
+    std::cin >> y;
+    if (x % y == 0)
+    {
+        std::cout << "Ctarno";
+    }
+    else;
+    {
+        std::cout << "Not Ctarno";
+    }
+    */
+
+    //file 1 task 8
+
+    /*
+    int num = 0;
+    std::cout << "Print a x";
+    std::cin >> num;
+    if (num % 3 == 0)
+    {
+        std::cout << "Ctarno 3";
+    }
+    else if (num % 5 == 0)
+    {
+        std::cout << "Ctarno 5";
+    }
+    else if (num % 7 == 0)
+    {
+        std::cout << "Ctarno 7";
+    }
+    else;
+    {
+        std::cout << "Not Ctarno";
+    }
+    */
+
+
+    //file 1 task 10
+
+    /*
+    int num = 0;
+    std::cout << "Print a x";
+    std::cin >> num;
+    if (num >= 1000)
+    {
+        std::cout << "Number have 4";
+    }
+    else if (num >= 100)
+    {
+        std::cout << "Number have 3";
+    }
+    else if (num >= 10)
+    {
+        std::cout << "Number have 2";
+    }
+    else;
+    {
+        std::cout << "Number have 1";
+    }
+    */
+
+    //file 1 task 11
+
+    /*
+    int numMain = 0;
+    std::cout << "Print a 5 NUMBER ";
+    std::cin >> numMain;
+
+    int num1 = numMain / 10000;
+    int num2 = (numMain / 1000) % 10;
+    int num3 = (numMain / 100) % 10;
+    int num4 = (numMain / 10) % 10;
+    int num5 = numMain % 10;
+
+    if (num1 == num5 && num2 == num4)
+    {
+        std::cout << "Number have 4";
+    }
+    else;
+    {
+        std::cout << "Ne palindrom";
+    }
+    */
+}
+
